3c1e3e67fb3c64d58a9e3efa1604f79269d9d23b
Nikhil Gupta <me@nikhgupta.com>
2015-04-08 16:51:01 +0530

    customized rails, using `creation` gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/monitor'
   end
